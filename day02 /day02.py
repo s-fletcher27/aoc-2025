61ab9c65d66851a69a10c39a006b528a0f49c564
s-fletcher27 <sgb.fletcher@gmail.com>
2025-12-02 13:24:23 +0000

    feat: solve day 2 part 2

--- day02 /day02.py
+++ day02 /day02.py
@@ -9,7 +9,7 @@ def load_ranges(filename: str):
     return ranges
 
 
-def is_invalid_id(n: int) -> bool:
+def is_invalid_id_part1(n: int) -> bool:
     s = str(n)
     if len(s) % 2 != 0:
         return False
@@ -17,15 +17,37 @@ def is_invalid_id(n: int) -> bool:
     return s[:half] == s[half:]
 
 
-def solve_day2(filename: str):
+def is_invalid_id_part2(n: int) -> bool:
+    s = str(n)
+    length = len(s)
+    for i in range(1, length // 2 + 1):
+        if length % i == 0:
+            subsection = s[:i]
+            if subsection * (length // i) == s:
+                return True
+    return False
+
+
+def solve_part1(filename: str):
     ranges = load_ranges(filename)
     total = 0
     for start, end in ranges:
         for n in range(start, end + 1):
-            if is_invalid_id(n):
+            if is_invalid_id_part1(n):
+                total += n
+    return total
+
+
+def solve_part2(filename: str):
+    ranges = load_ranges(filename)
+    total = 0
+    for start, end in ranges:
+        for n in range(start, end + 1):
+            if is_invalid_id_part2(n):
                 total += n
     return total
 
 
 if __name__ == "__main__":
-    print("Answer:", solve_day2("day02_input.txt"))
+    print("Part 1:", solve_part1("day02_input.txt"))
+    print("Part 2:", solve_part2("day02_input.txt"))
